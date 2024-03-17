a4252a2e3a986494d75a0f393ba226001e222ef1
Karol Żuraniewski <zuraniewski.karol@gmail.com>
2024-03-17 23:23:45 +0100

    feat(ui): product request minimal delay

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,9 +1,13 @@
 import { apiClient } from '@/httpCommon';
 import { ProductsResponse } from '@/types';
+import { delayPromise } from '@/utils';
 
 export const getProductsByPage = async (page: number, id?: number | null) => {
-	const response = await apiClient.get<ProductsResponse>('/products', {
-		params: { page, id },
-	});
+	const response = await delayPromise(
+		apiClient.get<ProductsResponse>('/products', {
+			params: { page, id },
+		}),
+	);
+
 	return response.data;
 };
---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+/**
+ * Ensures a minimal delay for specified promise
+ */
+export const delayPromise = async <T>(
+	promise: Promise<T>,
+	minDelay: number = 300,
+) => {
+	const [response] = await Promise.all([
+		promise,
+		new Promise((resolve) => setTimeout(resolve, minDelay)),
+	]);
+	return response;
+};
